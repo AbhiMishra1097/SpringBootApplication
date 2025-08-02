045f6950536882e5e21a3da01707ec30e1c275bc
AbhiMishra1097 <abhimishra1097@gmail.com>
2025-08-02 14:05:33 +0530

    refactor code

--- src/main/java/com/abhishek/projects/api/TestAPI.java
+++ src/main/java/com/abhishek/projects/api/TestAPI.java
@@ -2,12 +2,10 @@ package com.abhishek.projects.api;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
 @RequestMapping("/myapp")
 public interface TestAPI {
 
     @GetMapping("test-app")
-    public String testApplication();
+    String testApplication();
 }
--- src/main/java/com/abhishek/projects/controller/TestController.java
+++ src/main/java/com/abhishek/projects/controller/TestController.java
@@ -1,7 +1,9 @@
 package com.abhishek.projects.controller;
 
 import com.abhishek.projects.api.TestAPI;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class TestController implements TestAPI {
 
     public String testApplication() {
